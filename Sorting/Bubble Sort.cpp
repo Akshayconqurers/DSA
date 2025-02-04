5592ef8670ad2519ea322cfb31ed4cebf395f4da
Akshay Pratap Singh <117713892+Akshayconqurers@users.noreply.github.com>
2025-02-04 23:55:31 +0530

    Update Bubble Sort.cpp
    It is an unoptimized version as in this if no swap has occurred then also the pass will run.

--- Sorting/Bubble Sort.cpp
+++ Sorting/Bubble Sort.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+using namespace std;
 
+void bubbleSort(int arr[], int n){
+    
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1;j++){
+            if (arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+//function to print array
+void printArray(int arr[], int n){
+    
+    for(int i=0; i<n; i++){
+        cout<< arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    int arr[] = {4, 10, 12, 6, 8, 14, 20, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    cout << "Unsorted Array: ";
+    printArray(arr, n);
+    
+    bubbleSort(arr, n);
+    
+    cout << "Sorted Array: ";
+    printArray(arr, n);
+    
+    return 0;
+}
